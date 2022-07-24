35a17e2476737dc0b8b3fb25611d8474b491d56e
Ege Oral <egeoral@gmail.com>
2022-07-24 15:35:40 +0300

    Environment created.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float thrustSpeed = 1000f;
+
+    [SerializeField] float rotateLeftRightSpeed = 100f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerThrust();
+        PlayerRotation();
+    }
+
+    private void PlayerThrust()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            //rb.AddRelativeForce(0, 1f * thrustSpeed * Time.deltaTime, 0);
+            // OR
+            rb.AddRelativeForce(Vector3.up * thrustSpeed * Time.deltaTime);
+        }
+    }
+
+    private void PlayerRotation()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotateLeftRightSpeed);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotateLeftRightSpeed);
+        }
+    }
+
+    private void ApplyRotation(float rotationSpeed)
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
+}
